0a2c237853ef95c0dde2b983d43b5709960b1f06
Amir Hadzic <amir.hadzic@randomshouting.com>
2019-04-13 21:35:08 +0200

    Fix more prop-types (codemod missed some)

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,7 @@
 /* eslint react/prefer-stateless-function: 0 */
 
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import classNames from 'classnames';
 import { mapToCssModules } from './utils';
 
