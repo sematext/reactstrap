053f0c4645a05e1ac5597b4a076eed16be934764
Amir Hadzic <amir.hadzic@randomshouting.com>
2019-04-17 07:32:14 +0200

    Use react-transition-group

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { mapToCssModules } from './utils';
 
 const FirstChild = ({ children }) => (
